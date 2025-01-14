22fad4d252381d0688707f333c8bda7298f12f56
Виталий Шовкун <vetalshovkun@gmail.com>
2025-01-14 20:04:20 +0200

    Створено домашню сторінку. Додайте однакову висоту для карток продукту, та реалізайте детальний перегляд курсу (з кнопкою придбати)

--- ShopITCourses/Controllers/HomeController.cs
+++ ShopITCourses/Controllers/HomeController.cs
@@ -1,21 +1,28 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using ShopITCourses.Data;
 using ShopITCourses.Models;
+using ShopITCourses.Models.ViewModel;
 
 namespace ShopITCourses.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _db;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext db)
         {
             _logger = logger;
+            _db = db;
         }
 
         public IActionResult Index()
         {
-            return View();
+            HomeVM homeVM = new HomeVM();
+            homeVM.Products = _db.Product;
+            homeVM.Categorys = _db.Category;
+            return View(homeVM);
         }
 
         public IActionResult Privacy()
