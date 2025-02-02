c55e4a9b38e8a695dedf453b520fc22e8c7071bd
Виталий Шовкун <vetalshovkun@gmail.com>
2025-02-02 12:49:36 +0200

    Додано Api

---
+++ ShopITCourses/Controllers/Api/ProductController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ShopITCourses.Data;
+using ShopITCourses.Services.IServices;
+
+namespace ShopITCourses.Controllers.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _db;
+
+        public ProductController(ILogger<HomeController> logger, ApplicationDbContext db)
+        {
+            _logger = logger;
+            _db = db;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("All dsfsdfds OK");
+        }
+    }
+}
